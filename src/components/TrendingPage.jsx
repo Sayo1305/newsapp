1bce997a6d0084af14369f27c2f93d308960b022
priyal <priyaljaiin52@gmail.com>
2023-01-06 20:30:30 +0530

    changes to navbar

--- src/components/TrendingPage.jsx
+++ src/components/TrendingPage.jsx
@@ -27,7 +27,7 @@ const TrendingPage = () => {
         <div className="text-2xl font-semibold">trending On news App</div>
       </div>
       {/* nav bar  */}
-      <div>
+      {/* <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">
@@ -100,7 +100,63 @@ const TrendingPage = () => {
             </div>
           </div>
         </nav>
-        
+         */}
+         <div>
+         <nav class="bg-white shadow-lg">
+			<div class="max-w-6xl mx-auto px-4">
+				<div class="flex justify-between">
+					<div class="flex space-x-7">
+					
+						
+						
+						{/* <!-- Primary Navbar items --> */}
+						<div class="hidden md:flex items-center space-x-20">
+							<a href="" class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">Home</a>
+							<a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Popular</a>
+							<a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">International</a>
+							<a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Politics</a>
+              <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Business</a>
+              <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Science</a>
+              <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">IT</a>
+            </div>
+					</div>
+					
+					{/* <!-- Mobile menu button --> */}
+					<div class="md:hidden flex items-center">
+						<button class="outline-none mobile-menu-button">
+						<svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
+							x-show="!showMenu"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M4 6h16M4 12h16M4 18h16"></path>
+						</svg>
+					</button>
+					</div>
+				</div>
+			</div>
+			{/* <!-- mobile menu --> */}
+			<div class="hidden mobile-menu">
+				<ul class="">
+					<li class="active"><a href="index.html" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Popular</a></li>
+          <li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">International</a></li>
+					<li><a href="#services" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Politics</a></li>
+					<li><a href="#about" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Sports</a></li>
+					<li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Business</a></li>
+          <li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Science</a></li>
+          <li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">IT</a></li>
+
+				</ul>
+			</div>
+	
+		</nav>
+		
+	
+
       </div>
 
 
@@ -116,16 +172,15 @@ const TrendingPage = () => {
           </>
         ))}
       </div>
-   
+   <div id="testimonials">
      <div id="testimonials-carousel" class="carousel slide" data-ride="False">
      <div class="carousel-inner ">
       {
         data.map((mp  , idx)=>(
           <div class="carousel-item active ">
-          <h2 class="testimonial-title">I no longer have to sniff other dogs for love. I've found the hottest Corgi on
-            TinDog. Woof.</h2>
-          <img class="testimonial-img" src={mp.urlToImage} alt="dog-profile"/>
-          <em>Pebbles, New York</em>
+          <h2 class="testimonial-title">{mp.title}</h2>
+          <img class="testimonial-img" src={mp.urlToImage} alt="Content-img"/>
+          <em className="text-black h-2.5">{mp.content}</em>
  
         </div>
         ))
@@ -139,6 +194,7 @@ const TrendingPage = () => {
      </button>
    </div>
 
+   </div>
    </div>
   );
 };
